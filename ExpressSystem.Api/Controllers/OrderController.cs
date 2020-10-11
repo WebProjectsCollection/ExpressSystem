a8dab355e0886c364821df341b34af5ada30e0d8
Spencer Yang <Spencer_Yang@Jabil.com>
2020-10-11 18:41:52 +0800

    添加快递单导入

--- ExpressSystem.Api/Controllers/OrderController.cs
+++ ExpressSystem.Api/Controllers/OrderController.cs
@@ -84,5 +84,84 @@ namespace ExpressSystem.Api.Controllers
             bool re = OrderBLL.UpdateStatusByBatchNumber(batchNumber, action);
             return re ? MyResult.OK() : MyResult.Error();
         }
+        [Produces("application/json")]
+        [Consumes("application/json", "multipart/form-data")]
+        [HttpPost("importOrder")]
+        public IActionResult importOrder(IFormFile file, [FromQuery]string userName)
+        {
+            try
+            {
+                DataTable dt = ExcelHelper.ReadExcelToDataTable(file.OpenReadStream(), "Sheet1");
+
+                string sql = @"INSERT INTO tmp_importorder
+                                (`BatchID`,
+                                `ORDER_NUM`,
+                                `SENDER_PHONE`,
+                                `SENDER_NAME`,
+                                `SENDER_ADDRESS`,
+                                `JBBW_PHONE`,
+                                `JBBW_NAME`,
+                                `JBBW_ADDRESS`,
+                                `BATCH_NUMBER`,
+                                `CreatedBy`) VALUES ";
+                List<string> values = new List<string>();
+                string batchId = Guid.NewGuid().ToString("N");
+                string batchNumber = DateTime.Now.ToString("第yyyyMMdd批");
+                foreach (DataRow row in dt.Rows)
+                {
+                    string orderNum = Convert.ToString(row["快递单号"]);
+                    string jbbwPhone = Convert.ToString(row["津巴布韦电话"]);
+                    string jbbwAddress = Convert.ToString(row["津巴布韦地址"]);
+                    string jbbwName = Convert.ToString(row["津巴布韦姓名"]);
+                    string senderName = Convert.ToString(row["寄件人姓名"]);
+                    string senderPhone = Convert.ToString(row["寄件人电话"]);
+                    string senderAddress = Convert.ToString(row["寄件人地址"]);
+
+                    values.Add($"('{batchId}','{orderNum}','{senderPhone}','{senderName}','{senderAddress}','{jbbwPhone}','{jbbwName}','{jbbwAddress}','{batchNumber}','{userName}')");
+                }
+                if (values.Count == 0)
+                {
+                    throw new MsgException("导入人员信息为空！");
+                }
+                JabMySqlHelper.ExecuteNonQuery(Config.DBConnection, sql + string.Join(",", values));
+
+                DataTable resultdt = JabMySqlHelper.ExecuteDataTable(Config.DBConnection, $"call Sp_confirmImportOrder('{batchId}')");
+                List<OrderInfo> recordList = new List<OrderInfo>();
+                if (resultdt != null && resultdt.Rows.Count > 0)
+                {
+                    foreach (DataRow row in resultdt.Rows)
+                    {
+                        recordList.Add(new OrderInfo()
+                        {
+                            OrderNumber = Converter.TryToString(row["ORDER_NUM"]),
+                            JBBWPhone = Converter.TryToString(row["JBBW_PHONE"]),
+                            JBBWName = Converter.TryToString(row["JBBW_NAME"]),
+                            JBBWAddress = Converter.TryToString(row["JBBW_ADDRESS"]),
+                            SenderName = Converter.TryToString(row["SENDER_NAME"]),
+                            SenderPhone = Converter.TryToString(row["SENDER_PHONE"]),
+                            SenderAddress = Converter.TryToString(row["SENDER_ADDRESS"]),
+                            Status = Converter.TryToString(row["Status"]),
+                            BatchNo = Converter.TryToString(row["BATCH_NUMBER"]),
+                        });
+                    }
+                }
+                JabMySqlHelper.ExecuteNonQuery(Config.DBConnection, $"delete from tmp_importorder where BatchID = '{batchId}'");
+                return Ok(new
+                {
+                    status = 200,
+                    msg = $"导入完成，今日包裹批次号：{batchNumber}",
+                    data = recordList
+                });
+            }
+            catch (Exception e)
+            {
+                return Ok(new
+                {
+                    status = 500,
+                    msg = "导入失败:" + e.Message,
+                    error = "导入失败:" + e.Message + Environment.NewLine + e.StackTrace
+                });
+            }
+        }
     }
 }
